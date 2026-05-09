d0f1cc82251a84e8f7aeb93fc9fe826b3bd2f8b2
Patrick Eberle <pe@angstdoc.de>
2026-05-09 16:47:23 +0200

    Vercel Analytics auf install.html aktivieren (Cache v59)

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -8,7 +8,7 @@ importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 
 // === Cache Service Worker ===
 
-const CACHE_NAME = 'loewenherz-v58';
+const CACHE_NAME = 'loewenherz-v59';
 
 const URLS_TO_CACHE = [
   './',
